23358f3b04e2cf109a56fd728dc6e77bf8d9e78f
it-techmonkey <adityahazari14@gmail.com>
2026-08-28 11:18:16 +0530

    Removed surfaces section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
       </Reveal>
       <CategoriesStrip />
       <About />
-      <Surfaces />
+      {/* <Surfaces /> */}
       <LiveEvents />
       <EmailSignup />
       <Instagram />
